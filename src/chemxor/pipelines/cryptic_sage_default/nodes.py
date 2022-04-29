5b8376910531310a1fc5306182f13d713ec73bb7
Ankur Kumar <ank@leoank.me>
2022-04-29 21:51:21 +0530

    update cryptic sage pipeline
    Signed-off-by: Ankur Kumar <ank@leoank.me>

--- src/chemxor/pipelines/cryptic_sage_default/nodes.py
+++ src/chemxor/pipelines/cryptic_sage_default/nodes.py
@@ -7,12 +7,31 @@ from chemxor.data_modules.osm import OSMDataModule
 from chemxor.model.cryptic_sage import CrypticSage
 
 
-def train_model() -> None:
+def init_osm_data_module() -> pl.LightningDataModule:
+    return OSMDataModule()
+
+
+def init_cryptic_sage_model() -> pl.LightningModule:
+    return CrypticSage()
+
+
+def train_cryptic_sage_model(
+    osm_data_module: pl.LightningDataModule,
+    cryptic_sage_model: pl.LightningDataModule,
+) -> None:
     """Train model node funciton."""
-    osm = OSMDataModule()
-    model = CrypticSage()
-    trainer = pl.Trainer(limit_train_batches=100, max_epochs=1)
-    trainer.fit(model=model, datamodule=osm)
+    trainer = pl.Trainer(limit_train_batches=100)
+    trainer.fit(model=cryptic_sage_model, datamodule=osm_data_module)
 
 
-train_model_node = node(func=train_model, inputs=None, outputs=None)
+init_osm_data_module_node = node(
+    func=init_osm_data_module, inputs=None, outputs="osm_data_module"
+)
+init_cryptic_sage_model_node = node(
+    func=init_cryptic_sage_model, inputs=None, outputs="cryptic_sage_model"
+)
+train_cryptic_sage_model_node = node(
+    func=train_cryptic_sage_model,
+    inputs=["osm_data_module", "cryptic_sage_model"],
+    outputs=None,
+)
--- src/chemxor/pipelines/cryptic_sage_default/pipeline.py
+++ src/chemxor/pipelines/cryptic_sage_default/pipeline.py
@@ -3,9 +3,16 @@
 from kedro.pipeline import Pipeline
 
 from chemxor.pipelines.cryptic_sage_default.nodes import (
-    prepare_dataset_node,
-    train_model_node,
+    init_cryptic_sage_model_node,
+    init_osm_data_module_node,
+    train_cryptic_sage_model_node,
 )
 
 # Assemble nodes into a pipeline
-cryptic_sage_default_pipeline = Pipeline([prepare_dataset_node, train_model_node])
+cryptic_sage_default_pipeline = Pipeline(
+    [
+        init_osm_data_module_node,
+        init_cryptic_sage_model_node,
+        train_cryptic_sage_model_node,
+    ]
+)
