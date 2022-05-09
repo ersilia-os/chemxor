f24510503632117b4f33efee6cc1b7167a4d1abd
Ankur Kumar <ank@leoank.me>
2022-05-09 14:27:57 +0530

    update test enc route
    Signed-off-by: Ankur Kumar <ank@leoank.me>

--- src/chemxor/routes/test_encryption.py
+++ src/chemxor/routes/test_encryption.py
@@ -1,7 +1,13 @@
 """Test encryption route."""
 
-from flask import Blueprint
-from flask import Response
+from http.client import OK
+import json
+from http import HTTPStatus
+
+from flask import Blueprint, make_response, Response, request
+from pydantic import parse_obj_as, ValidationError
+
+from chemxor.schema.test_encryption import TestEncPost, TestEncResponse
 
 test_encryption_bp = Blueprint("test_encryption", __name__, url_prefix="/v1/test")
 
@@ -13,4 +19,16 @@ def test_encryption_post() -> Response:
     Returns:
         Response: Flask response
     """
-    return "OK"
+    # Parse json from requests
+    try:
+        test_enc = parse_obj_as(TestEncPost, json.loads(request.data))
+    except ValidationError:
+        response_body = TestEncResponse(vector=[])
+        return make_response(response_body.json(), HTTPStatus.BAD_REQUEST)
+    except Exception:
+        response_body = TestEncResponse(vector=[])
+        return make_response(response_body.json(), HTTPStatus.INTERNAL_SERVER_ERROR)
+
+    # Perform addition
+    new_vector = test_enc.vector + [1, 2, 3]
+    return make_response(TestEncResponse(vector=new_vector).json(), HTTPStatus.OK)
