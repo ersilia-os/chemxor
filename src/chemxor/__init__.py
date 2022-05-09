093cf4279df7548c45620a7ed44c86d74c529e17
Ankur Kumar <ank@leoank.me>
2022-05-09 14:00:07 +0530

    add version
    Signed-off-by: Ankur Kumar <ank@leoank.me>

--- src/chemxor/__init__.py
+++ src/chemxor/__init__.py
@@ -1,6 +1,13 @@
 """ChemXor."""
 
+from importlib.metadata import PackageNotFoundError, version  # type: ignore
+
 from warnings import filterwarnings
+
+try:
+    __version__ = version(__name__)
+except PackageNotFoundError:  # pragma: no cover
+    __version__ = "unknown"
 
 filterwarnings(action="ignore", category=DeprecationWarning, module="tensorboard")
 filterwarnings(action="ignore", category=DeprecationWarning, module="torchvision")
