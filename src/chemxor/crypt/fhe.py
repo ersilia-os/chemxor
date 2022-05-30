9ebf2dfc7c9ee838993d624a7f0dceb12df10620
Ankur Kumar <ank@leoank.me>
2022-05-30 11:08:22 +0530

    Add tensor enc and dec functions
    Signed-off-by: Ankur Kumar <ank@leoank.me>

--- src/chemxor/crypt/fhe.py
+++ src/chemxor/crypt/fhe.py
@@ -5,6 +5,7 @@ from types import MethodType
 from typing import Any, Union
 
 import pytorch_lightning as pl
+import tenseal as ts
 from torch import nn, Tensor
 
 from chemxor.crypt.onnx_utils import onnx_to_torch_fhe_forward, torch_to_onnx
@@ -13,9 +14,13 @@ from chemxor.crypt.onnx_utils import onnx_to_torch_fhe_forward, torch_to_onnx
 class FHECryptor:
     """FHE for AI/ML models."""
 
-    def __init__(self: "FHECryptor", context: Any) -> None:
-        """Setup cryptographic params."""
-        pass
+    def __init__(self: "FHECryptor", context: ts.Context) -> None:
+        """Setup cryptographic params.
+
+        Args:
+            context (ts.Context): TenSeal Context
+        """
+        self.context = context
 
     def load_model(
         self: "FHECryptor", model: bytes
@@ -34,16 +39,52 @@ class FHECryptor:
         model: Union[nn.Module, pl.LightningModule],
         dummy_input: Any,
     ) -> Union[nn.Module, pl.LightningModule]:
-        """Encrypt the model."""
+        """Convert the model to be FHE compatible.
+
+        Args:
+            model (Union[nn.Module, pl.LightningModule]): Model to convert
+            dummy_input (Any): Dummy input
+
+        Returns:
+            Union[nn.Module, pl.LightningModule]: Converted model
+        """
         onnx_model = torch_to_onnx(model, dummy_input)
         modified_forward = onnx_to_torch_fhe_forward(onnx_model, dummy_input)
         model.forward = MethodType(modified_forward, model)
         return model
 
     def encrypt_tensor(self: "FHECryptor", tensor: Tensor) -> Tensor:
-        """Encrypt the tensor."""
-        pass
+        """Encrypt the tensor.
 
-    def decrypt_tensor(self: "FHECryptor", tensor: Tensor) -> Tensor:
-        """Decrypt the tensor."""
-        pass
+        Args:
+            tensor (Tensor): Tensor to encrypt
+
+        Raises:
+            Exception: Public key not found
+
+        Returns:
+            Tensor: Encrypted tensor
+        """
+        if self.context.has_public_key() is False:
+            raise Exception("Public key not found in the context")
+        else:
+            encryptor = self.context.encryptor()
+            return encryptor(tensor)
+
+    def decrypt_tensor(self: "FHECryptor", enc_tensor: Tensor) -> Tensor:
+        """Decrypt the tensor.
+
+        Args:
+            enc_tensor (Tensor): Encrypted tensor
+
+        Raises:
+            Exception: Secret key not found
+
+        Returns:
+            Tensor: Decrypted tensor
+        """
+        if self.context.has_secret_key() is False:
+            raise Exception("Secret key not found in the context")
+        else:
+            decryptor = self.context.decryptor()
+            return decryptor(enc_tensor)
