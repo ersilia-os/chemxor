95dc7a4c09eddb21cdfba9fda2e70c201861fb4c
Ankur Kumar <ank@leoank.me>
2022-05-30 11:46:54 +0530

    add ops palceholder
    Signed-off-by: Ankur Kumar <ank@leoank.me>

--- src/chemxor/crypt/fhe_torch_ops.py
+++ src/chemxor/crypt/fhe_torch_ops.py
@@ -5,3 +5,159 @@ from typing import Tuple
 
 def fhe_torch_add() -> Tuple:
     pass
+
+
+def fhe_torch_abs():
+    pass
+
+
+def fhe_torch_acos():
+    pass
+
+
+def fhe_torch_acosh():
+    pass
+
+
+def fhe_torch_asin():
+    pass
+
+
+def fhe_torch_asinh():
+    pass
+
+
+def fhe_torch_atan():
+    pass
+
+
+def fhe_torch_atanh():
+    pass
+
+
+def fhe_torch_celu():
+    pass
+
+
+def fhe_torch_clip():
+    pass
+
+
+def fhe_torch_constant():
+    pass
+
+
+def fhe_torch_cos():
+    pass
+
+
+def fhe_torch_cosh():
+    pass
+
+
+def fhe_torch_div():
+    pass
+
+
+def fhe_torch_elu():
+    pass
+
+
+def fhe_torch_equal():
+    pass
+
+
+def fhe_torch_erf():
+    pass
+
+
+def fhe_torch_exp():
+    pass
+
+
+def fhe_torch_gemm():
+    pass
+
+
+def fhe_torch_greater():
+    pass
+
+
+def fhe_torch_hardsigmoid():
+    pass
+
+
+def fhe_torch_identity():
+    pass
+
+
+def fhe_torch_leakyrelu():
+    pass
+
+
+def fhe_torch_less():
+    pass
+
+
+def fhe_torch_log():
+    pass
+
+
+def fhe_torch_matmul():
+    pass
+
+
+def fhe_torch_mul():
+    pass
+
+
+def fhe_torch_not():
+    pass
+
+
+def fhe_torch_relu():
+    pass
+
+
+def fhe_torch_reshape():
+    pass
+
+
+def fhe_torch_selu():
+    pass
+
+
+def fhe_torch_sigmoid():
+    pass
+
+
+def fhe_torch_sin():
+    pass
+
+
+def fhe_torch_sinh():
+    pass
+
+
+def fhe_torch_softplus():
+    pass
+
+
+def fhe_torch_sub():
+    pass
+
+
+def fhe_torch_tan():
+    pass
+
+
+def fhe_torch_tanh():
+    pass
+
+
+def fhe_torch_thresholdedrelu():
+    pass
+
+
+def torch_conv():
+    pass
