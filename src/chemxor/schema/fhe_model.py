f5848d22a534af7de9f30797d43a0b55e98ad2b6
Ankur Kumar <ank@leoank.me>
2022-05-30 10:02:02 +0530

    add fhe api schema
    Signed-off-by: Ankur Kumar <ank@leoank.me>

---
+++ src/chemxor/schema/fhe_model.py
@@ -0,0 +1,16 @@
+"""FHE Encrypted model API schema."""
+
+from pydantic import BaseModel, Field
+
+
+class FHEModelQueryPostRequest(BaseModel):
+    """FHE Encrypted model query post request schema."""
+
+    ts_context: str = Field(unicode_safe=False)
+    input_tensor: str = Field(unicode_safe=False)
+
+
+class FHEModelQueryPostResponse(BaseModel):
+    """FHE Encrypted model query post response schema."""
+
+    output_tensor: str = Field(unicode_safe=False)
