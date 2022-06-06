d2604fe2fc947286b53eda81662751b165a4e795
Ankur Kumar <ank@leoank.me>
2022-06-06 18:14:02 +0530

    Add enc dataset wrapper
    Signed-off-by: Ankur Kumar <ank@leoank.me>

---
+++ src/chemxor/data_modules/enc_dataset.py
@@ -0,0 +1,45 @@
+"""Encrypted Dataset wrapper."""
+
+from typing import Any
+
+import tenseal as ts
+from torch.utils.data import Dataset
+
+
+class EncDataset(Dataset):
+    """Encrypted Dataset."""
+
+    def __init__(self: "EncDataset", context: ts.Context, dataset: Dataset) -> None:
+        """Encrypted dataset.
+
+        Args:
+            context (ts.Context): Tenseal encryption context.
+            dataset (Dataset): Initialized dataset class to wrap.
+
+        """
+        self.context = context
+        self.dataset = dataset
+
+    def __len__(self: "EncDataset") -> int:
+        """Length of the dataset.
+
+        Returns:
+            int: Length of the dataset.
+        """
+        return self.dataset.__len__()
+
+    def __getitem__(self: "EncDataset", index: int) -> Any:
+        """Get item from the dataset.
+
+        Args:
+            index (int): index of the item.
+
+        Returns:
+            Any: Item
+        """
+        items = self.dataset.__getitem__(index)
+        # Encrypt items
+        enc_item_list = []
+        for item in items:
+            enc_item_list.append(ts.ckks_tensor(self.context, item))
+        return tuple(enc_item_list)
