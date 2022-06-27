05336c21a854a75e97d9eb2fa216b1c1514bfcc6
Ankur Kumar <ank@leoank.me>
2022-06-27 13:29:46 +0530

    Add convnet pipeline
    Signed-off-by: Ankur Kumar <ank@leoank.me>

--- src/chemxor/pipeline_registry.py
+++ src/chemxor/pipeline_registry.py
@@ -2,7 +2,9 @@
 from typing import Dict
 
 from kedro.pipeline import Pipeline
-
+from chemxor.pipelines.convnet_default.pipeline import (
+    convnet_default_pipeline,
+)
 from chemxor.pipelines.cryptic_sage_default.pipeline import (
     cryptic_sage_default_pipeline,
 )
@@ -17,6 +19,7 @@ def register_pipelines() -> Dict[str, Pipeline]:
         A mapping from a pipeline name to a ``Pipeline`` object.
     """
     return {
+        "convnet": convnet_default_pipeline,
         "cryptic_sage_default": cryptic_sage_default_pipeline,
         "iris_split": iris_split_pipeline,
         "__default__": hello_pipeline + iris_split_pipeline,
