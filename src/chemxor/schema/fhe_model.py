3cd8594d16980e9d4667c5ef9a863e98e827b45f
Ankur Kumar <ank@leoank.me>
2022-07-29 08:10:35 +0530

    update schema
    Signed-off-by: Ankur Kumar <ank@leoank.me>

--- src/chemxor/schema/fhe_model.py
+++ src/chemxor/schema/fhe_model.py
@@ -1,18 +1,38 @@
 """FHE Encrypted model API schema."""
 
-from typing import Optional
+from enum import Enum
+from typing import List, Optional
 
 from pydantic import BaseModel, Field
+
+
+class PreProcessInput(Enum):
+    """Supported input pre processor."""
+
+    IM_TO_COL = "im_to_col"
 
 
 class FHEModelQueryPostRequest(BaseModel):
     """FHE Encrypted model query post request schema."""
 
     ts_context: str = Field(unicode_safe=False)
-    input_tensor: str = Field(unicode_safe=False)
+    model_input: str = Field(unicode_safe=False)
 
 
 class FHEModelQueryPostResponse(BaseModel):
     """FHE Encrypted model query post response schema."""
 
-    output_tensor: Optional[str] = Field(unicode_safe=False)
+    model_output: Optional[str] = Field(unicode_safe=False)
+
+
+class PartFHEModelQueryPostRequest(FHEModelQueryPostRequest):
+    """Partitioned FHE Encrypted model query post request schema."""
+
+    model_step: Optional[int]
+
+
+class PartFHEModelQueryPostResponse(FHEModelQueryPostResponse):
+    """Partitioned FHE Encrypted model query post response schema."""
+
+    next_step: int
+    preprocess_next_input: Optional[List[PreProcessInput]] = None
